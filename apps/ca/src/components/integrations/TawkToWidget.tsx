ba2892b8e05b8c72b6ff80f27cdc8ed86578bc8c
Sahil Jain <sahiljainsj004@gmail.com>
2026-06-18 15:10:34 +0530

    feat: add TawkToWidget component with lazy-loaded script and custom responsive positioning logic

--- apps/ca/src/components/integrations/TawkToWidget.tsx
+++ apps/ca/src/components/integrations/TawkToWidget.tsx
@@ -5,13 +5,10 @@ import { runAfterEngagement } from "@/lib/loadOnEngagement";
 
 const tawkToScript = `
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-Tawk_API.customStyle = {
-  zIndex: 50
-};
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/68e4120082c311194f82b349/1j6teckkr';
+s1.src='https://embed.tawk.to/67be09f37739ba190cd1649c/1ikv5dom5';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
